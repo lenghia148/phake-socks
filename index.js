1c7c8f7050a0858a85709bcd94234c38abfdfbb0
DESKTOP-2JOBADM\Nghia <ledinhnghia.qb@gmail.com>
2020-11-15 18:09:48 +0700

    update

--- index.js
+++ index.js
@@ -1,34 +1,159 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+var itemsFirst = [
+    {
+        name : 'item/1.webp',
+        style : 'a'
+    },
+    {
+        name : 'item/2.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/3.jpg',
+        style : 'a',
+    },
+    {
+        name : 'item/4.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/5.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/6.jpg',
+        style : 'a',
+    },
+    {
+        name : 'item/7.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/8.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/9.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/10.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/11.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/12.webp',
+        style : 'a',
+    },
+]
+loadDataFirst()
+function loadDataFirst()
+{   
+    let dataFirst = ''
+    
+    for(let i = 0 ; i < itemsFirst.length ; i++)
+    {
+    let item = `<div class='items'><img src="${itemsFirst[i].name}"> <div>${itemsFirst[i].style}</div></div>`
+    dataFirst+=item
+    }
+    console.log(dataFirst)
+    document.getElementById('first').innerHTML = dataFirst
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+listnews = [
+    {
+        link : 'http://bongdaso.com/Mourinho-s%e1%ba%b5n-s%c3%a0ng-ch%e1%ba%a5m-d%e1%bb%a9t-h%e1%bb%a3p-%c4%91%e1%bb%93ng-s%e1%bb%9bm-v%e1%bb%9bi-Fernandes-_Art_223510.aspx'
+    },
+    {
+        link : 'http://bongdaso.com/Mourinho-s%e1%ba%b5n-s%c3%a0ng-ch%e1%ba%a5m-d%e1%bb%a9t-h%e1%bb%a3p-%c4%91%e1%bb%93ng-s%e1%bb%9bm-v%e1%bb%9bi-Fernandes-_Art_223510.aspx'
+    },
+    {
+        link : 'http://bongdaso.com/Mourinho-s%e1%ba%b5n-s%c3%a0ng-ch%e1%ba%a5m-d%e1%bb%a9t-h%e1%bb%a3p-%c4%91%e1%bb%93ng-s%e1%bb%9bm-v%e1%bb%9bi-Fernandes-_Art_223510.aspx'
+    },
+  
+]
+loadDataSecond()
+function loadDataSecond()
+{   
+    let dataSecond = ''
+    
+    for(let i = 0 ; i < listnews.length ; i++)
+    {
+    let item = `<div class='listnews'><img src='${itemsFirst[i].name}'><a href="${listnews[i].link}"> Link ${i} </a></div>`
+    dataSecond+=item
+    }
+   
+    document.getElementById('second').innerHTML = dataSecond
+}
+var itemsThird = [
+    {
+        name : 'item/1.webp',
+        style : 'a'
+    },
+    {
+        name : 'item/2.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/3.jpg',
+        style : 'a',
+    },
+    {
+        name : 'item/4.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/5.webp',
+        style : 'a',
+    },
+    {
+        name : 'item/6.jpg',
+        style : 'a',
+    },
+    {
+        name : 'item/3.jpg',
+        style : 'a',
+    },
+    {
+        name : 'item/4.webp',
+        style : 'a',
+    },
+]
+loadDataThird()
+function loadDataThird()
+{   
+    let dataThird = ''
+    
+    for(let i = 0 ; i < itemsThird.length ; i++)
+    {
+    let item = `<div class='trends'><img src='${itemsThird[i].name}'>${i}</div>`
+    dataThird+=item
+    }
+   
+    document.getElementById('third').innerHTML = dataThird
+}
+
+var imgSlides = document.getElementsByClassName('imgss');
+var indexImgs = 0;
+showSlides(0)
+function nextImgs() {
+    indexImgs++;
+    if(indexImgs>1){indexImgs=0}
+  showSlides(indexImgs);
+}
+function prevImgs() {
+    indexImgs--;
+    if(indexImgs>0){indexImgs=1}
+  showSlides(indexImgs);
+}
+
+function showSlides(n) {
+  for (i = 0; i < imgSlides.length; i++) {
+      imgSlides[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
-}var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+  imgSlides[n].style.display = 'block'; 
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
 
